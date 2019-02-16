3eaf46323ab14f42571cc610e6ed508f27ac013e
Rob Roos <1robroos@gmail.com>
2019-02-16 22:52:54 +0100

    S3-statefile backend definition

---
+++ chapter3/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "kfsolutions-terraform-up-and-running-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kfsolutions-terraform-up-and-running-state"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
