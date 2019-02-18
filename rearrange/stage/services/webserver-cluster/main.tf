cafb112b96e9ca400f047734963964c0ae5820a8
Rob Roos <1robroos@gmail.com>
2019-02-18 21:56:23 +0100

    Adding terraform_remote_state data source

--- rearrange/stage/services/webserver-cluster/main.tf
+++ rearrange/stage/services/webserver-cluster/main.tf
@@ -1,22 +1,23 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_instance" "example" {
+  ami                    = "ami-40d28157"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
-#resource "aws_instance" "example" {
-#  ami                    = "ami-40d28157"
-#  instance_type          = "t2.micro"
-#  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-#
-#  user_data = <<-EOF
-#              #!/bin/bash
-#              echo "Hello, World" > index.html
-#              nohup busybox httpd -f -p "${var.server_port}" &
-#              EOF
-#
-#  tags {
-#    Name = "terraform-example"
-#  }
-#}
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              echo "${data.terraform_remote_state.db.address}" >> index.html
+              echo "${data.terraform_remote_state.db.port}" >> index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  tags {
+    Name = "terraform-example_withuserdata"
+  }
+}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -29,14 +30,26 @@ resource "aws_security_group" "instance" {
   }
 }
 
-#output "public_ip" {
-#  value = "${aws_instance.example.public_ip}"
-#}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
 
 terraform {
-  backend "s3" {
+ backend "s3" {
+   bucket = "kfsolutions-terraform-up-and-running-state"
+   key    = "stage/services/webserver-cluster/terraform.tfstate"
+   region = "us-east-1"
+
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config {
     bucket = "kfsolutions-terraform-up-and-running-state"
-    key    = "stage/services/webserver-cluster/terraform.webserver.tfstate"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-1"
   }
 }
