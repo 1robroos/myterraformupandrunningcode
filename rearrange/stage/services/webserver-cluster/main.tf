04b0eb49ea7c8ec7e804cdadaa2333e3a2a2b9eb
Rob Roos <1robroos@gmail.com>
2019-02-19 09:23:37 +0100

    userdata in template file , calling rendered output of the template_file data source

--- rearrange/stage/services/webserver-cluster/main.tf
+++ rearrange/stage/services/webserver-cluster/main.tf
@@ -1,21 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "example" {
   ami                    = "ami-40d28157"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              echo "${data.terraform_remote_state.db.address}" >> index.html
-              echo "${data.terraform_remote_state.db.port}" >> index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  user_data              = "${data.template_file.user_data.rendered}"
 
   tags {
     Name = "terraform-example_withuserdata"
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.address}"
+    db_port     = "${data.terraform_remote_state.db.port}"
   }
 }
 
@@ -30,17 +34,15 @@ resource "aws_security_group" "instance" {
   }
 }
 
-
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
 
 terraform {
- backend "s3" {
-   bucket = "kfsolutions-terraform-up-and-running-state"
-   key    = "stage/services/webserver-cluster/terraform.tfstate"
-   region = "us-east-1"
-
+  backend "s3" {
+    bucket = "kfsolutions-terraform-up-and-running-state"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
